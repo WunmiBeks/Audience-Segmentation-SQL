858f422f3a10e73332bcb25826f1fd0deca48361
WunmiBeks <wunmi.cloud@gmail>
2025-09-18 00:02:13 -0600

    Anniversary Journey sql code

---
+++ 4-Anniversary/3-OneYear_MembershipAnniversary - UpdateFlag.sql
@@ -0,0 +1,5 @@
+SELECT      Journey.MemberID,
+            1 AS HasClaimedDiscount
+FROM        [OneYear_Anniversary_JourneyEntry] Journey
+INNER JOIN  [Redemption_Log] Redeem 
+ON          Journey.MemberID = Redeem.MemberID
